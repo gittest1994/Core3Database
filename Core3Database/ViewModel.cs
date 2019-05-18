b012c7cfa499b3b403aa5a9283f3740c689c1c31
Mahdi Hosseini <mahdidvb72@gmail.com>
2019-05-18 17:30:11 +0430

    Add project files.

---
+++ Core3Database/ViewModel.cs
@@ -0,0 +1,41 @@
+﻿using GalaSoft.MvvmLight;
+using GalaSoft.MvvmLight.Command;
+using HandyControl.Controls;
+using HandyControl.Data;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Core3Database
+{
+    public class ViewModel : ViewModelBase
+    {
+        public RelayCommand<FunctionEventArgs<object>> OpenViewCMD => new Lazy<RelayCommand<FunctionEventArgs<object>>>(() => new RelayCommand<FunctionEventArgs<object>>(OpenView)).Value;
+
+        private void OpenView(FunctionEventArgs<object> obj)
+        {
+            var item = obj.Info as SideMenuItem;
+            if(obj.Info is SideMenuItem)
+            {
+                switch (item.Header)
+                {
+                    case "Add User":
+                        MainWindow.main.ChangeContent(new AddView());
+                        break;
+                    case "Edit User":
+                        MainWindow.main.ChangeContent(new EditView());
+                        break;
+                    case "View Data":
+                        MainWindow.main.ChangeContent(new GetView());
+
+                        break;
+                    case "Delete User":
+                        MainWindow.main.ChangeContent(new EditView());
+
+                        break;
+                }
+            }
+
+        }
+    }
+}
